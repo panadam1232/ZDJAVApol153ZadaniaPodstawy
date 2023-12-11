088e98c914bb8ea004ea850e59612608af9aaaaf
AdamPomalecki <adam.pomalecki@gmail.com>
2023-12-11 18:41:36 +0100

    poprawione rozwiazanie zadanie7

--- src/main/java/pl/sdacademy/java/basic/excercises/day1/Zadanie7.java
+++ src/main/java/pl/sdacademy/java/basic/excercises/day1/Zadanie7.java
@@ -1,4 +1,7 @@
 package pl.sdacademy.java.basic.excercises.day1;
+
+import java.util.Scanner;
+
 /*
 Napisz program realizujący prosty kalkulator. Program powinien:
 a) pobrać pierwszą liczbę (typu float);
@@ -14,36 +17,52 @@ działania programu nastąpi po wpisaniu tekstu „Stop”.
  */
 public class Zadanie7 {
     public static void main(String[] args) {
-        calculate(8,2,'+');
-        calculate(8,2,'-');
-        calculate(8,0,'/');
-        calculate(8,2,'*');
+        calculate(8, 2, "+");
+        calculate(8, 2, "-");
+        calculate(8, 2, "/");
+        calculate(8, 2, "*");
+        calculate(8, 2, "?");
 
+        boolean wantToContinue = true;
+        while (wantToContinue) {
+            Scanner keyboardScanner = new Scanner(System.in);
+            System.out.print("Podaj pierwszą liczbę: ");
+            float a = keyboardScanner.nextFloat();
+            System.out.print("Podaj drugą liczbę: ");
+            float b = keyboardScanner.nextFloat();
+            System.out.print("Podaj operator +, -, /, *: ");
+            String c = keyboardScanner.next();
+            calculate(a, b, c);
+            System.out.print("Czy chcesz kontynuować: \n Wpisz true jeśli chcesz kontynuować \n Wpisz false jeśli nie chcesz kontynuować \nWpisz wartość: ");
+            wantToContinue = keyboardScanner.nextBoolean();
+        }
     }
 
-    private static void calculate (int a, int b, char c){
-        int sum;
-        switch (c){
-            case '+':
+    private static void calculate(float a, float b, String c) {
+        float sum;
+        switch (c) {
+            case "+":
                 sum = a + b;
                 System.out.println(a + " + " + b + " = " + sum);
-                        break;
-            case '-':
+                break;
+            case "-":
                 sum = a - b;
                 System.out.println(a + " - " + b + " = " + sum);
                 break;
-            case '/':
+            case "/":
                 if (a == 0 || b == 0) {
-                    System.out.println("Nie dziel przez 0!");
+                    System.out.println("Wrong operation");
                     break;
                 }
                 sum = a / b;
                 System.out.println(a + " / " + b + " = " + sum);
                 break;
-            case '*':
+            case "*":
                 sum = a * b;
                 System.out.println(a + " * " + b + " = " + sum);
                 break;
+            default:
+                System.out.println("„Operator  not exist");
         }
     }
 }
