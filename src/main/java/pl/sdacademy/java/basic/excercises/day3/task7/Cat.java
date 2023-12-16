9a343ae73531624ee31d0e0c1d5afa6dbf9d4f35
AdamPomalecki <adam.pomalecki@gmail.com>
2023-12-16 13:19:27 +0100

    rozwiazanie zadania 7

---
+++ src/main/java/pl/sdacademy/java/basic/excercises/day3/task7/Cat.java
@@ -0,0 +1,47 @@
+package pl.sdacademy.java.basic.excercises.day3.task7;
+
+import java.util.Objects;
+
+public class Cat {
+    private String name;
+
+    public Cat(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    void makeSound() {
+        System.out.println("Miau Miau");
+    }
+
+    void eatMice(int mice) {
+        System.out.println("I ate " + mice + " mice.");
+    }
+
+    void print() {
+        System.out.println(this);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cat cat = (Cat) o;
+        return Objects.equals(name, cat.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/pl/sdacademy/java/basic/excercises/day3/task7/Main.java
@@ -0,0 +1,23 @@
+package pl.sdacademy.java.basic.excercises.day3.task7;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Cat[] cats = new Cat[3];
+
+        cats[0] = new Cat("Filemon");
+        cats[1] = new Cat("Bonifacy");
+        cats[2] = new Cat("Mruczek");
+
+        int mice = 5;
+        for (Cat cat: cats ){
+            System.out.println(cat.toString());
+            System.out.println(cat);
+            cat.print();
+            cat.makeSound();
+            cat.eatMice(mice++ * mice);
+            System.out.println("***");
+        }
+
+    }
+}
